cbdeb36e61bb6d481701c7f58b65f76609ba5fc9
vladradcuk5 <vladradcuk5@gmail.com>
2026-09-08 19:26:33 +0300

    Add personal information script

---
+++ about.py
@@ -0,0 +1,14 @@
+# Program: personal information card
+def main():
+    name = "Vlad"
+    surname = "Radchuk"
+    group = "IT-32"
+    birth_year = 2009
+
+    print(f"Name: {name} {surname}")
+    print(f"Group: {group}")
+    print(f"Age in 2026: {2026 - birth_year}")
+    print("Favourite language: Python")
+
+
+main()
